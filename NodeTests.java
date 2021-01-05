e2d6a99b680513f6b0f29d71a7ebb711658615c2
Tami Takada <tami@mytalos.com>
2021-01-05 14:49:48 -0500

    added tests for setData

--- NodeTests.java
+++ NodeTests.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class NodeTests {
   public static void main(String[] args) {
     testNodeCreationAndGet();
+    testDataSet();
   }
 
   public static void testNodeCreationAndGet() {
@@ -33,6 +34,33 @@ public class NodeTests {
     Utils.showRandomResults(failInfo);
   }
 
+  public static void testDataSet() {
+    ArrayList<Boolean> results = new ArrayList<Boolean>();
 
+    Node one = new Node("CATS!");
+    results.add(one.getData().equals("CATS!"));
+
+    one.setData("DOGS!");
+    results.add(one.getData().equals("DOGS!"));
+
+    one.setData("");
+    results.add(one.getData().equals(""));
+
+    ArrayList<String> failInfo = new ArrayList<String>();
+
+    for (int i = 0; i < 100; i++) {
+      String str = Utils.createRandomString();
+      Node test = new Node("");
+      test.setData(str);
+      if (!test.getData().equals(str)) {
+        failInfo.add(str);
+        break;
+      }
+    }
+
+    results.add(failInfo.isEmpty());
+    Utils.showResults(results, "Test Set Data");
+    Utils.showRandomResults(failInfo);
+  }
 
 }
